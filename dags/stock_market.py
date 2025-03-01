a1476c1f2b45f4eaffc048b7446cc971a9cd1198
Justin David <justindavid2000@gmail.com>
2025-03-01 16:12:23 +0400

    formatting stock prices with Spark and DockerOperator

--- dags/stock_market.py
+++ dags/stock_market.py
@@ -2,6 +2,7 @@ from airflow.decorators import dag, task
 from airflow.hooks.base import BaseHook
 from airflow.sensors.base import PokeReturnValue
 from airflow.operators.python import PythonOperator
+from airflow.providers.docker.operators.docker import DockerOperator
 from datetime import datetime
 
 from include.stock_market.tasks import _get_stock_prices
@@ -39,7 +40,23 @@ def stock_market():
         python_callable = _store_prices,
         op_kwargs = {'stock': '{{ ti.xcom_pull(task_ids="get_stock_prices") }}'}
     )
+
+    format_prices = DockerOperator(
+        task_id = 'format_prices',
+        image = 'airflow/stock-app',
+        container_name = 'format_prices',
+        api_version = 'auto',
+        auto_remove = 'success',
+        docker_url = 'tcp://docker-proxy:2375',
+        network_mode = 'container:spark-master',
+        tty = True,
+        xcom_all = False,
+        mount_tmp_dir = False,
+        environment = {
+            'SPARK_APPLICATION_ARGS': '{{ ti.xcom_pull(task_ids="store_prices") }}'
+        }
+    )
     
-    is_api_available() >> get_stock_prices >> store_prices
+    is_api_available() >> get_stock_prices >> store_prices >> format_prices
 
 stock_market()
