4df68096dd4151a0f6ad7f69255edaa41ce5f5a9
Justin David <justindavid2000@gmail.com>
2025-03-01 16:50:41 +0400

    storing in posgres database

--- include/stock_market/tasks.py
+++ include/stock_market/tasks.py
@@ -2,7 +2,19 @@ from airflow.hooks.base import BaseHook
 import  json
 from io import BytesIO
 from minio import Minio
+from airflow.exceptions import AirflowNotFoundException
 
+BUCKET_NAME = 'stock-market'
+
+def _get_minio_clent():
+    minio = BaseHook.get_connection('minio')
+    client = Minio(
+        endpoint=minio.extra_dejson['endpoint_url'].split('//')[1],
+        access_key=minio.login,
+        secret_key=minio.password, 
+        secure=False
+    )
+    return client
 # These are the tasks executed by the DAG - stock_market.py
 
 # Gets Stock Prices and outputs the data as JSON
@@ -17,23 +29,25 @@ def _get_stock_prices(url, symbol):
 
 # Store Prices Function returns the path to the stored object in the format required in the MinIO Storage
 def _store_prices(stock):
-    minio = BaseHook.get_connection('minio')
-    client = Minio(
-        endpoint=minio.extra_dejson['endpoint_url'].split('//')[1],
-        access_key=minio.login,
-        secret_key=minio.password, 
-        secure=False
-    )
-    bucket_name = 'stock-market'
-    if not client.bucket_exists(bucket_name):
-        client.make_bucket(bucket_name)
+    client = _get_minio_clent()
+    if not client.bucket_exists(BUCKET_NAME):
+        client.make_bucket(BUCKET_NAME)
     stock = json.loads(stock)
     symbol = stock['meta']['symbol']
     data = json.dumps(stock, ensure_ascii=False).encode('utf8')
     obgw = client.put_object(
-        bucket_name = bucket_name, 
+        bucket_name = BUCKET_NAME, 
         object_name = f'{symbol}/prices.json',
         data = BytesIO(data),
         length = len(data)
         )
     return f'{obgw.bucket_name}/{symbol}'
+
+def _get_formatted_csv(path):
+    client = _get_minio_clent()
+    prefix_name = f"{path.split('/')[1]}/formatted_prices/"
+    objects = client.list_objects(BUCKET_NAME, prefix=prefix_name, recursive = True)
+    for obj in objects:
+        if obj.object_name.endswith('.csv'):
+            return obj.object_name
+    return AirflowNotFoundException(f'No CSV file found')
